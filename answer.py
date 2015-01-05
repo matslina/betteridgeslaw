cc60296448bd8978807d12d7f44292d8434d1432
Mats Linander <matslina@gmail.com>
2015-01-04 21:49:22 -0500

    tool for answering betteridgian headlines

---
+++ answer.py
@@ -0,0 +1,84 @@
+import sys
+import select
+import tty
+import termios
+import os
+import os.path
+import re
+import random
+import cPickle
+
+
+SAMPLE_SIZE = 100
+
+
+def load_headlines():
+    headlines = {}
+    for f in os.listdir('.'):
+        m = re.match(r'state_(.*)Crawler.pkl$', f)
+        if not m:
+            continue
+        source = m.group(1)
+        print "Loading", source
+        state = cPickle.load(open(f))
+        headlines[source] = state['articles'].keys()
+    return headlines
+
+def load_sample():
+    if os.path.exists('sample.pkl'):
+        return cPickle.load(open('sample.pkl'))
+
+    headlines = load_headlines()
+    sample = []
+    for source in headlines:
+        dubious = [hline for hline in headlines[source] if hline.endswith('?')]
+        if len(dubious) < SAMPLE_SIZE:
+            print ("Warning: only found %d (<%d) dubious headlines for %s" %
+                   (len(dubious), SAMPLE_SIZE, source))
+        size = min(SAMPLE_SIZE, len(dubious))
+        sample.extend({'source': source,
+                       'title': title}
+                      for title in random.sample(dubious, size))
+
+    random.shuffle(sample)
+
+    return sample
+
+def write_sample(sample):
+    f = open('sample.pkl.tmp', 'w')
+    cPickle.dump(sample, f)
+    f.close()
+    os.rename('sample.pkl.tmp', 'sample.pkl')
+
+def main():
+    sample = load_sample()
+    unanswered = [hline for hline in sample if 'answer' not in hline]
+    options = ''.join('\x1b[1m' + c + '\x1b[0m' if c.isupper() else c
+                       for c in '[Yes / No / Maybe / Polarity fail / Quit]')
+    stdin_attrs = termios.tcgetattr(sys.stdin)
+    try:
+        tty.setcbreak(sys.stdin.fileno())
+
+        for hline in sample:
+            if 'answer' in hline:
+                continue
+
+            print options, hline['title']
+
+            c = None
+            while True:
+                if select.select([sys.stdin], [], [], 1) == ([sys.stdin], [], []):
+                    c = sys.stdin.read(1).lower()
+                    break
+
+            if c in ('y', 'n', 'm', 'p'):
+                hline['answer'] = c.lower()
+            if c.lower() in ('q', ''):
+                break
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, stdin_attrs)
+
+    write_sample(sample)
+
+if __name__ == "__main__":
+    sys.exit(main())
